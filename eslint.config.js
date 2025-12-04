5b38806667031cd548b5ad9cd9abef97848e0f2e
Sebastian Werner <s.werner@sebastian-software.de>
2025-12-04 16:22:39 +0100

    chore: upgrade to react 19 and storybook 10
    - React 18 → 19
    - Storybook 8.6 → 10.1
    - Add storybook-static to .gitignore
    - Migrate storybook config (addon-essentials → addon-docs)

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,25 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 import globals from "globals"
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node
     }
-  },
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-      ]
-    }
-  },
-  {
-    ignores: ["dist/", "node_modules/", "storybook-static/"]
   }
-)
+}, {
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+    ]
+  }
+}, {
+  ignores: ["dist/", "node_modules/", "storybook-static/"]
+}, storybook.configs["flat/recommended"]);
 
