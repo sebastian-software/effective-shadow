75ff9d3c5ed648f1523be0550dc84e2073c1d761
Sebastian Werner <s.werner@sebastian-software.de>
2025-12-04 15:00:21 +0100

    chore: add eslint flat config with typescript support

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import globals from "globals"
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+      ]
+    }
+  },
+  {
+    ignores: ["dist/", "node_modules/", "storybook-static/"]
+  }
+)
+
